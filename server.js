ffdca06bb35608390127a5f1472c28390a829468
Bram Van Mensel <bram.vanmensel@nxtport.eu>
2020-05-22 01:01:03 +0200

    fix cors

--- server.js
+++ server.js
@@ -35,8 +35,10 @@ const allowedOrigins = [
   "http://localhost:5000",
   "http://projecthelios.azurewebsites.net",
   "http://bram-lab.com",
+  "http://station-service.bram-lab.com",
   "https://projecthelios.azurewebsites.net",
-  "https://bram-lab.com"
+  "https://bram-lab.com",
+  "https://station-service.bram-lab.com"
 ]
 
 const corsOptions = {
